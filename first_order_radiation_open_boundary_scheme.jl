774a617c67b4fce3b2434864c3eb3a515cecf778
tomaschor <tomaschor@gmail.com>
2024-07-17 16:01:22 -0600

    first order scheme improved

--- first_order_radiation_open_boundary_scheme.jl
+++ first_order_radiation_open_boundary_scheme.jl
@@ -2,7 +2,7 @@ using Oceananigans.Grids: xspacing, yspacing, zspacing
 using Oceananigans.BoundaryConditions: Open, getbc
 import Oceananigans.BoundaryConditions: _fill_west_open_halo!, _fill_east_open_halo!, _fill_south_open_halo!, _fill_north_open_halo!, _fill_bottom_open_halo!, _fill_north_open_halo!
 
-mutable struct FirstOrderRadiation{FT}
+struct FirstOrderRadiation{FT}
     relaxation_timescale :: FT
     c⁻¹
 end
@@ -30,7 +30,7 @@ end
 const C = Center()
 
 @inline function _fill_west_open_halo!(j, k, grid, c, bc::FOROBC, loc, clock, model_fields)
-    gradient_free_c = @inbounds model_fields.u⁻¹[2, j, k]
+    gradient_free_c = @inbounds bc.classification.matching_scheme.c⁻¹[1, j, k]
     @inbounds c[1, j, k] = relax(j, k, gradient_free_c, bc, grid, clock, model_fields)
 
     return nothing
@@ -39,7 +39,6 @@ end
 @inline function _fill_east_open_halo!(j, k, grid, c, bc::FOROBC, loc, clock, model_fields)
     i = grid.Nx + 1
 
-    gradient_free_c = @inbounds model_fields.u⁻¹[i - 1, j, k]
     gradient_free_c = @inbounds bc.classification.matching_scheme.c⁻¹[1, j, k]
     @inbounds c[i, j, k] = relax(j, k, gradient_free_c, bc, grid, clock, model_fields)
 
@@ -47,7 +46,7 @@ end
 end
 
 @inline function _fill_south_open_halo!(i, k, grid, c, bc::FOROBC, loc, clock, model_fields)
-    gradient_free_c = @inbounds model_fields.v⁻¹[i, 2, k]
+    gradient_free_c = @inbounds bc.classification.matching_scheme.c⁻¹[1, j, k]
     @inbounds c[i, 1, k] = relax(i, k, gradient_free_c, bc, grid, clock, model_fields)
 
     return nothing
@@ -56,14 +55,14 @@ end
 @inline function _fill_north_open_halo!(i, k, grid, c, bc::FOROBC, loc, clock, model_fields)
     j = grid.Ny + 1
 
-    gradient_free_c = @inbounds model_fields.v⁻¹[i, j - 1, k]
+    gradient_free_c = @inbounds bc.classification.matching_scheme.c⁻¹[1, j, k]
     @inbounds c[i, j, k] = relax(i, k, gradient_free_c, bc, grid, clock, model_fields)
 
     return nothing
 end
 
 @inline function _fill_bottom_open_halo!(i, j, grid, c, bc::FOROBC, loc, clock, model_fields)
-    gradient_free_c = @inbounds model_fields.w⁻¹[i, j, 2]
+    gradient_free_c = @inbounds bc.classification.matching_scheme.c⁻¹[1, j, k]
     @inbounds c[i, j, 1] = relax(i, j, gradient_free_c, bc, grid, clock, model_fields)
 
     return nothing
@@ -72,8 +71,22 @@ end
 @inline function _fill_top_open_halo!(i, j, grid, c, bc::FOROBC, loc, clock, model_fields)
     k = grid.Nz + 1
 
-    gradient_free_c = @inbounds model_fields.w⁻¹[i, j, k - 1]
+    gradient_free_c = @inbounds bc.classification.matching_scheme.c⁻¹[1, j, k]
     @inbounds c[i, j, k] = relax(i, j, gradient_free_c, bc, grid, clock, model_fields)
 
     return nothing
+end
+
+using Oceananigans: prognostic_fields
+function update_first_order_radiation_matching_scheme!(sim)
+    fields = prognostic_fields(sim.model)
+    for (field_name, field) in zip(keys(fields), values(fields))
+        bcs = field.boundary_conditions
+        bcs.east   isa FOROBC && (interior(sim.model.velocities.u.boundary_conditions.east.classification.matching_scheme.c⁻¹,   1, :, :) .= interior(sim.model.velocities.u, grid.Nx, :, :))
+        bcs.west   isa FOROBC && (interior(sim.model.velocities.u.boundary_conditions.west.classification.matching_scheme.c⁻¹,   1, :, :) .= interior(sim.model.velocities.u, 2, :, :))
+        bcs.south  isa FOROBC && (interior(sim.model.velocities.v.boundary_conditions.south.classification.matching_scheme.c⁻¹,  :, 1, :) .= interior(sim.model.velocities.v, :, grid.Ny, :))
+        bcs.north  isa FOROBC && (interior(sim.model.velocities.v.boundary_conditions.north.classification.matching_scheme.c⁻¹,  :, 1, :) .= interior(sim.model.velocities.v, :, 2, :))
+        bcs.bottom isa FOROBC && (interior(sim.model.velocities.w.boundary_conditions.bottom.classification.matching_scheme.c⁻¹, :, :, 1) .= interior(sim.model.velocities.w, :, :, grid.Nz))
+        bcs.top    isa FOROBC && (interior(sim.model.velocities.w.boundary_conditions.top.classification.matching_scheme.c⁻¹,    :, :, 1) .= interior(sim.model.velocities.w, :, :, 2))
+    end
 end
